2626961a99f5b04a981a2fe8d73e3b60189f29ec
JuanCRB <juanchocarlos98@gmail.com>
2021-01-10 11:53:51 -0600

    cambios de subnav
    del aprtado superior de todas las vistas principales

--- resources/views/layouts/subnav.blade.php
+++ resources/views/layouts/subnav.blade.php
@@ -3,15 +3,22 @@
     <a class="nav-link" href="{{ route('home') }}" style="color: #000;">Inicio
       <i class="fa fa-home m-auto btn" style="color: #ff922b;"></i>
     </a>
-    <a class="nav-link" href="{{ route('pedimentosVista') }}" style="color: #000;">Pedimentos
-      <i class="fa fa-ship m-auto btn" style="color: #ff922b;"></i>
+    <a class="nav-link" href="{{ route('pedimentosVista') }}" style="color: #000;">CRM
+      <i class="fas fa-handshake m-auto btn" style="color: #ff922b;"></i>
     </a>
-    <a class="nav-link" href="{{ route('facturasVista') }}" style="color: #000;">Facturación
-      <i class="fa fa-coins m-auto btn" style="color: #ff922b;"></i>
+    <a class="nav-link" href="{{ route('facturasVista') }}" style="color: #000;">Pedidos
+      <i class="fas fa-file-signature m-auto btn" style="color: #ff922b;"></i>
     </a>
-    <a class="nav-link" href="{{ route('documentosVista') }}" style="color: #000;">Documentos
-      <i class="fa fa-folder m-auto btn" style="color: #ff922b;"></i>
+    <a class="nav-link" href="{{ route('documentosVista') }}" style="color: #000;">MRP
+      <i class="fas fa-warehouse m-auto btn" style="color: #ff922b;"></i>
     </a>
+    <a class="nav-link" href="{{ route('documentosVista') }}" style="color: #000;">Productos
+      <i class="fas fa-boxes m-auto btn" style="color: #ff922b;"></i>
+    </a>
+    <a class="nav-link" href="{{ route('documentosVista') }}" style="color: #000;">Ofertas
+      <i class="fas fa-chart-bar m-auto btn" style="color: #ff922b;"></i>
+    </a>
+
     <div class="navbar-nav mr-auto">
     </div>
     <form class="form-inline mt-2 mt-md-0" style="margin:10px">
