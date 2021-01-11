7663d5f68ce0c6c9e179751b4ea59e2162b2573e
glencruz <43675388+glencruz@users.noreply.github.com>
2021-01-11 17:02:25 -0600

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/BrayanCetina/ERP into main"""
    This reverts commit d88b45154f4bcebec7792b3d46a8d33f65331a3e.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
       <div class="col-lg-4">
         <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
           <div class="features-icons-icon d-flex">
-            <a class="m-auto"  href="{{ route('clientesVista') }}">
+            <a class="m-auto"  href="{{ route('pedimentosVista') }}">
               <i class="fas fa-handshake m-auto btn" style="color: #ff922b;"></i>
             </a>
           </div>
